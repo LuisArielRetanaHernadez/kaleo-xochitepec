526d91126ff3471b4d8131baa0a8a1e4eb6b0873
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-10-26 13:34:58 -0600

    agregando la configuracion de router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1 >Menu</h1>,
+    children: [
+      {
+        path: '/home',
+        index: true,
+        element: <h1>Home</h1>
+      },
+      {
+        path: '/group',
+        element: <h1>Contact</h1>
+      },
+      {
+        path: '/event',
+        element: <h1>Events</h1>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <StrictMode >
+    <RouterProvider router={router} />
+  </StrictMode>
 )
